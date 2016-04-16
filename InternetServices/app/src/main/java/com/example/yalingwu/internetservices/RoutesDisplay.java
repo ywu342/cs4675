5c6dea631b06dc5911f8da02284622608d1acaf9
nkarusala <nkarusala@gmail.com>
2016-04-16 18:51:07 -0400

    adding second screen

---
+++ InternetServices/app/src/main/java/com/example/yalingwu/internetservices/RoutesDisplay.java
@@ -0,0 +1,14 @@
+package com.example.yalingwu.internetservices;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RoutesDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_routes_display);
+
+    }
+}
